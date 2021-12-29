692cc99201847744d6c8a70ac0479d6d0e93d619
nianaaap <89662650+nianaaap@users.noreply.github.com>
2021-12-29 11:50:41 -0600

    Add files via upload

---
+++ 2. C++ ES GENIAL.cpp
@@ -0,0 +1,12 @@
+//2. C++ Es genial
+
+#include <iostream>
+using namespace std;
+
+int main (){
+	//Vamos a crear un programa que imprima C++ es genial
+	cout<< "C++ es genial";
+	
+	return 0;
+	
+}
