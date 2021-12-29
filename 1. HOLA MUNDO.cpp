6d0cca2fad40b5e9d87a562d96043afcaad769cb
nianaaap <89662650+nianaaap@users.noreply.github.com>
2021-12-29 11:16:27 -0600

    Add files via upload

---
+++ 1. HOLA MUNDO.cpp
@@ -0,0 +1,15 @@
+//Primer programa en C++
+
+// HOLA MUNDO
+
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	cout<< "¡Hola Mundo! ";
+	cout<< "Esto "<< " es " << " ¡increible" ;
+	return 0;
+	
+}
+
